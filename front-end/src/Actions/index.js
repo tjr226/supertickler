b0e16cc98598af698e77216033b89fef3f98e90b
tjr226 <tjr226@gmail.com>
2019-09-02 18:01:28 -0400

    deleted front end built with yarn

--- front-end/src/Actions/index.js
+++
@@ -1,1 +0,0 @@
-export * from './taskActions.js';
--- front-end/src/Reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { taskreducer } from './taskreducer';
-
-export default combineReducers({
-    taskreducer
-});
-
